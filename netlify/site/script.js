f0c5c13e9a7534f26187fa8100b65dd38ec5cda2
ibrahim-ashraf <ibrahimashrafabdo@gmail.com>
2024-03-24 03:10:09 +0200

    Recreate the registration page

--- netlify/site/script.js
+++ netlify/site/script.js
@@ -1,61 +1,55 @@
-function createVerificationCode() {
-      return Math.floor(Math.random() * 10);
-    }
-let verificationCode = "";
-for (let i = 0; i < 5; i++) {
-    verificationCode += createVerificationCode();
+const dateTime = new Date();
+
+function setBirthDate() {
+    const maxDate = new Date(dateTime.getFullYear() - 18, dateTime.getMonth(), dateTime.getDate() + 1 ).toISOString().split("T")[0];
+    const minDate = new Date(dateTime.getFullYear() - 30, dateTime.getMonth(), dateTime.getDate() + 1).toISOString().split("T")[0];
+
+    document.getElementById("birth_date").max = maxDate;
+    document.getElementById("birth_date").min = minDate;
+
+    document.getElementById("birth_date_min").textContent = maxDate;
+    document.getElementById("birth_date_max").textContent = minDate;
+}
+
+function createChallengeCode() {
+    let challengeCode = "";
+    let challengeCodeNumbers = Math.floor(Math.random() * 3) + 4;
+
+    for (let i = 0; i < challengeCodeNumbers; i++) {
+        challengeCode += Math.floor(Math.random() * 10);
     }
 
-document.getElementById("verification_code").value = verificationCode;
+    document.getElementById("challenge_code_numbers").textContent = challengeCodeNumbers;
+    document.getElementById("challenge_code").textContent = challengeCode;
+    document.getElementById("challenge_code_hidden").value = challengeCode;
 
-localStorage.setItem("registrationNumber", registrationNumber);
+    // localStorage.setItem("registrationNumber", registrationNumber);
+}
 
 function showOtherCountryInput() {
-    var countrySelect = document.getElementById("country");
-var otherCountryInput = document.getElementById("other_country");
-var nationalIdInput = document.getElementById("national_id");
-    // Get the country select element
-    var countrySelect = document.getElementById("country");
+    const countrySelect = document.getElementById("country");
+    const otherCountryInput = document.getElementById("other_country");
 
-    // Get the other country input element
-    var otherCountryInput = document.getElementById("other_country");
-
-    // Check the value of the country select element
-    if (countrySelect.value == "أخرى") {
-      // Show the other country input
-      otherCountryInput.style.display = "block";
-  
-      // Make the other country input required
-      otherCountryInput.required = true;
+    if (countrySelect.selectedIndex === countrySelect.options.length - 1) {
+        otherCountryInput.style.display = "block";
+        otherCountryInput.required = true;
     } else {
-      // Hide the other country input
-      otherCountryInput.style.display = "none";
-
-      // Make the other country input not required
-      otherCountryInput.required = false;
+        otherCountryInput.style.display = "none";
+        otherCountryInput.required = false;
     }
-  
-  // Check the value of the country select element
-  if (countrySelect.value == "مصر") {
-    // Set the placeholder of the national ID input to "الرقم القومي"
-    nationalIdInput.placeholder = "الرقم القومي";
-  } else {
-    // Set the placeholder of the national ID input to "رقم بطاقة الهوية الوطنية"
-    nationalIdInput.placeholder = "رقم بطاقة الهوية الوطنية";
-  }
 }
 
-function displayDateAndTime() {
-  const d = new Date();
-  const days = ["الأحد", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت"];
-  const months = ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
-  let dddd = days[d.getDay()];
-  let dd = d.getDate()
-  let mmmm = months[d.getMonth()];
-  let yyyy = d.getFullYear();
-  let date = dddd + "، " + dd + " " + mmmm + " " + yyyy;
-  let time = d.toLocaleTimeString()
-document.getElementById("date_time").innerHTML = date + "، " + time;
-}
+function displayDateTime() {
+    const days = ["الأحد", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت"];
+    const months = ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
 
-setInterval(displayDateAndTime, 1000);
+    let dddd = days[dateTime.getDay()];
+    let dd = dateTime.getDate();
+    let mmmm = months[dateTime.getMonth()];
+    let yyyy = dateTime.getFullYear();
+    let dateText = `${dddd}، ${dd} ${mmmm} ${yyyy}`;
+    let time = dateTime.toLocaleTimeString()
+    document.getElementById("date_time").innerHTML = `${date}، ${time}`;
+
+    setInterval(displayDateTime, 1000);
+}
