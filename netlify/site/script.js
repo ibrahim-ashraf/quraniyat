1cddc798f55c7e769ce4d4a061de15701ba996af
ibrahim-ashraf <ibrahimashrafabdo@gmail.com>
2024-04-11 23:30:07 +0200

    Update script.js

--- netlify/site/script.js
+++ netlify/site/script.js
@@ -1,6 +1,6 @@
 const supabaseUrl = "https://ovonkzsbmfrwtzjmhatb.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92b25renNibWZyd3R6am1oYXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3NjQ2MTIsImV4cCI6MjAyODM0MDYxMn0.eBlzqI4U3byFYss3P0zw4BCREg9YmN678L-NsoXhwqA";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 
 const dateTime = new Date();
