e619e01bf35b0cd78f8720fdf32c2a4d750c5094
ibrahim-ashraf <ibrahimashrafabdo@gmail.com>
2024-06-27 16:36:11 +0300

    Update script.js

--- netlify/site/script.js
+++ netlify/site/script.js
@@ -1,28 +1,28 @@
 function createChallengeCode() {
-    let challengeCode = "";
-    let challengeCodeNumbers = Math.floor(Math.random() * 3) + 4;
+  let challengeCode = "";
+  let challengeCodeNumbers = Math.floor(Math.random() * 3) + 4;
 
-    for (let i = 0; i < challengeCodeNumbers; i++) {
-        challengeCode += Math.floor(Math.random() * 10);
-    }
+  for (let i = 0; i < challengeCodeNumbers; i++) {
+    challengeCode += Math.floor(Math.random() * 10);
+  }
 
-    document.getElementById("challenge-code-hidden").value = challengeCode;
+  document.getElementById("challenge-code-hidden").value = challengeCode;
 
-    return challengeCode;
+  return challengeCode;
 }
 
 
 function displayDateTime() {
-    const days = ["الأحد", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت"];
-    const months = ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
+  const days = ["الأحد", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت"];
+  const months = ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
 
-    let dddd = days[dateTime.getDay()];
-    let dd = dateTime.getDate();
-    let mmmm = months[dateTime.getMonth()];
-    let yyyy = dateTime.getFullYear();
-    let dateText = `${dddd}، ${dd} ${mmmm} ${yyyy}`;
-    let time = dateTime.toLocaleTimeString();
-    document.getElementById("date_time").innerHTML = `${date}، ${time}`;
+  let dddd = days[dateTime.getDay()];
+  let dd = dateTime.getDate();
+  let mmmm = months[dateTime.getMonth()];
+  let yyyy = dateTime.getFullYear();
+  let dateText = `${dddd}، ${dd} ${mmmm} ${yyyy}`;
+  let time = dateTime.toLocaleTimeString();
+  document.getElementById("date_time").innerHTML = `${date}، ${time}`;
 
-    setInterval(displayDateTime, 1000);
+  setInterval(displayDateTime, 1000);
 }
