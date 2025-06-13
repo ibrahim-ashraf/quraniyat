8493d6bebc521418260c90878d568aec850cfefd
ibrahim-ashraf <ibrahimashrafabdo@gmail.com>
2025-06-13 15:34:51 +0300

    Delete config.js

--- netlify/public/config.js
+++
@@ -1,8 +0,0 @@
-// التكوينات والمتغيرات البيئية للواجهة الأمامية
-const config = {
-  // يمكن تحديث هذه القيم من خلال متغيرات البيئة في Netlify
-  API_BASE_URL: '/.netlify/functions',
-  GOOGLE_API_KEY: 'AIzaSyCVmRoMuxDyubg_VAiRCIIpWuZIcH-qzws'
-};
-
-export default config;
